66bd7d82fa65fee6847b3d82aa02fbfb30306518
sjparkjust-arch <sjparkjust@gmail.com>
2026-08-21 16:17:59 +0900

    infra: add ALB, target group, HTTP listener

---
+++ infra/alb.tf
@@ -0,0 +1,56 @@
+resource "aws_lb" "main" {
+  name               = "bobpick-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_c.id]
+
+  tags = {
+    Name = "bobpick-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "bobpick-app-tg"
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "bobpick-app-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.main.dns_name
+}
+
+output "alb_arn" {
+  value = aws_lb.main.arn
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.app.arn
+}
