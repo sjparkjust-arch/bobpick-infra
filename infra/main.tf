7b03b88caeafdf42e29cdee6f93d6da682ca5cf9
cheobonggi <hanbin69777@gmail.com>
2026-08-27 16:16:03 +0900

    roleback

--- infra/main.tf
+++ infra/main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "bobpick-terraform-state"
-    key          = "bobpick/terraform.tfstate"
-    region       = "ap-northeast-2"
-    use_lockfile = true
-    encrypt      = true
+    bucket         = "bobpick-terraform-state"
+    key            = "bobpick/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "bobpick-terraform-lock"
+    encrypt        = true
   }
 }
 
