4ca7d1063d18779a9f97f25525838245860a57e2
cheobonggi <hanbin69777@gmail.com>
2026-09-17 17:24:45 +0900

    thr

--- infra/oidc-irsa.tf
+++ infra/oidc-irsa.tf
@@ -43,3 +43,40 @@ resource "aws_iam_role_policy_attachment" "app_pod_s3_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess" # 필요에 따라 권한 축소 가능
   role       = aws_iam_role.app_pod_s3_role.name
 }
+
+# ==========================================
+# 3. AWS Load Balancer Controller용 IRSA
+# ==========================================
+# 다운로드 받은 JSON 파일로 IAM 정책 생성
+resource "aws_iam_policy" "albc_policy" {
+  name   = "AWSLoadBalancerControllerIAMPolicy-3rd"
+  policy = file("iam_policy.json")
+}
+
+# ALB 로봇이 사용할 IAM 역할(신분증) 생성
+resource "aws_iam_role" "albc_role" {
+  name = "bobpick-albc-role-3rd"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          # kube-system 공간에 설치될 aws-load-balancer-controller만 이 신분증을 쓸 수 있음
+          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+}
+
+# 역할(신분증)에 정책(권한) 연결
+resource "aws_iam_role_policy_attachment" "albc_policy_attach" {
+  policy_arn = aws_iam_policy.albc_policy.arn
+  role       = aws_iam_role.albc_role.name
+}
